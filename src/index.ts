6779302619cd8cb7588665fffeff7b5495d8c3ae
amalej <alejandroalejandre15@gmail.com>
2023-09-05 05:36:20 +0800

    Add --version

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import readline from "readline";
 import { FirebaseCommands } from "./firebase-cmd";
 import { CommandLineInterface } from "./cli";
 import { ChildProcess } from "child_process";
+import packageJSON from "./packageJSON";
 interface CommandConfig {
   label: string;
   usage: string;
@@ -486,5 +487,16 @@ class OnFireCLI extends CommandLineInterface {
   }
 }
 
-const cli = new OnFireCLI({ prefix: "> " });
-cli.init();
+function initializeApp() {
+  const cli = new OnFireCLI({ prefix: "> " });
+  if (process.argv.length <= 2) {
+    cli.init();
+  } else if (process.argv.includes("--version")) {
+    console.log(`OnFire: v${packageJSON.version}`);
+  } else {
+    console.log(`OnFire: Unknown args ${process.argv.slice(2).join(", ")}`);
+    console.log(`Type 'onfire' to initialize the OnFire CLI`);
+  }
+}
+
+initializeApp();
