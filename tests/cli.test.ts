00b594c838975f4b793dd47ddc0adbb5dbc4cf69
amalej <alejandroalejandre15@gmail.com>
2023-09-18 07:55:15 +0800

    Added additional tests

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -7,6 +7,7 @@ class MockCommandLineInterface extends CommandLineInterface {
   ): {
     base: string;
     args: { [key: string]: string };
+    flags: string[];
     options: string[];
     input: string[];
   } {
@@ -16,14 +17,21 @@ class MockCommandLineInterface extends CommandLineInterface {
   _textRed(str: string): string {
     return this.textRed(str);
   }
+
   _textGreen(str: string): string {
     return this.textGreen(str);
   }
+
   _textYellow(str: string): string {
     return this.textYellow(str);
   }
+
   _textBold(str: string): string {
     return this.textBold(str);
+  }
+
+  _capitalizeFirstLetter(str: string): string {
+    return this.capitalizeFirstLetter(str);
   }
 }
 
@@ -49,6 +57,11 @@ describe("Text formatting", () => {
   it("Should make the text bold", () => {
     const wrappedText = cli._textBold(text);
     expect(wrappedText).toBe(`\x1b[1m${text}\x1b[0m`);
+  });
+
+  it("Should capitalize the first letter of 'this is a sentence'", () => {
+    const wrappedText = cli._capitalizeFirstLetter("this is a sentence");
+    expect(wrappedText).toBe(`This is a sentence`);
   });
 });
 
@@ -86,5 +99,26 @@ describe("Deconstuct commands", () => {
     expect(options.includes("--debug")).toBe(true);
     expect(input[0]).toBe("bin_file");
     expect(args["--app"]).toBe("app_id");
+  });
+
+  it(`Should deconstuct command with '=' sign: 'appdistribution:distribute --debug --app=app_id bin_file --release-notes "This is the release notes" --project=demo-proj'`, () => {
+    const command = `appdistribution:distribute --debug --app=app_id bin_file --release-notes "This is the release notes" --project=demo-proj"`;
+    const { base, args, options, input } = cli._getCommandParams(command, [
+      "--debug",
+    ]);
+
+    expect(base).toBe("appdistribution:distribute");
+    expect(options.includes("--debug")).toBe(true);
+    expect(input[0]).toBe("bin_file");
+    expect(args["--app"]).toBe("app_id");
+    expect(args["--project"]).toBe("demo-proj");
+    expect(args["--release-notes"]).toBe(`"This is the release notes"`);
+  });
+
+  it("Should list all flags passed for 'appdistribution:distribute --debug --app app_id bin_file --project demo-proj'", () => {
+    const command = `appdistribution:distribute --debug --app app_id bin_file --project demo-proj`;
+    const { flags } = cli._getCommandParams(command, ["--debug"]);
+
+    expect(flags).toEqual(["--debug", "--app", "--project"]);
   });
 });
