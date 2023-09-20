e005835edf29ca8dbd9cb5f6f2309e38c0d2b0b4
amalej <alejandroalejandre15@gmail.com>
2023-09-21 03:28:58 +0800

    Added module-loader.js

---
+++ src/module-loader.js
@@ -0,0 +1,22 @@
+function stringify(obj) {
+    let cache = [];
+    let str = JSON.stringify(obj, function (key, value) {
+        if (typeof value === "object" && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                // Circular reference found, discard key
+                return;
+            }
+            // Store value in our collection
+            cache.push(value);
+        }
+        return value;
+    });
+    cache = null; // reset the cache
+    return str;
+}
+
+process.on('message', function (path) {
+    const client = require(path); // Load the Firebase Tools module
+    const configStr = stringify(client) // Removes circular reference and convert to string
+    process.send(configStr); // Send to main process
+});
